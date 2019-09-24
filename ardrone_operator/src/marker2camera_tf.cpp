ae6d280dfcd648e665bca3f67256aa4ea2df7496
yuya-onozuka <pleasure26327@gmail.com>
2019-09-24 23:13:13 +0900

    modify marker2camera_tf.cpp

--- ardrone_operator/src/marker2camera_tf.cpp
+++ ardrone_operator/src/marker2camera_tf.cpp
@@ -22,17 +22,17 @@ Eigen::Matrix4d Marker2CameraTransform::marker2camera_matrix(std::vector<cv::Poi
     cv::Rodrigues(rotation_vector, rotation_matrix);
 
     //create transform_matrix
-    Eigen::Matrix4d rt_matrix;
+    Eigen::Matrix4d rt_transform_matrix;
     for(int i = 0; i < 3; i++)
     {
         for(int j = 0; j < 3; j++)
         {
-            rt_matrix(i,j) = rotation_matrix.at<double>(i,j);
+            rt_transform_matrix(i,j) = rotation_matrix.at<double>(i,j);
         }
     }
     for(int i = 0; i < 3; i++)
     {
-        rt_matrix(i,3) = tanslation_vector.at<double>(i,1);
+        rt_transform_matrix(i,3) = tanslation_vector.at<double>(i,1);
     }
-    return rt_matrix;
+    return rt_transform_matrix;
 }
